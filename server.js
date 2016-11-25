fe5aa7f71db9cf6f18cfec385e393fe8c258ca1b
Shafaaf Hossain <shafaafhossain@Shafaafs-MacBook-Pro.local>
2016-11-25 10:32:52 -0500

    dfs

--- server.js
+++ server.js
@@ -1,20 +1,47 @@
+//Using MVC model for project folder structure
+//See this link to understand logic: http://stackoverflow.com/questions/5178334/folder-structure-for-a-node-js-project
+
+//Adding in dependencies
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var path = require('path');
 var io = require('socket.io')(http)
 
-require('./chatRoom.js')(app, http, io);
-//require('./app/routes.js')(app, sess);  //load our routes and pass in app
+//-----------------------------------------------------------------------
 
-//to include my custom css,javascript files 
-app.use(express.static('public'));
+//Setup application
 
-//Serve index.html page
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('public'));	//to use public folder for js, css stuff
+app.set('views', path.join(__dirname, 'views')); //add in views folder to find ejs files
+app.set('view engine', 'ejs'); // set up ejs for as templating engine
 
-//Listen for connections
+//app.set('controllers', path.join(__dirname, 'controllers')); //add in views folder to find ejs files
+
+//-----------------------------------------------------------------------
+
+//Controllers
+
+/* Team: If want to add in a feature, add it in like the example below.
+Pass in the app, etc objects to the file needed and add in route in the routes folder.
+There should not be any controller/logic code here as it would get messy
+*/
+
+//All routing logic
+require('./routes.js')(app);
+
+//Chat room logic
+
+//If get error here its because mongodb not installed on your machin, so comment out this line.
+require('./controllers/chatRoom.js')(app, http, io);  
+
+//...more features here
+
+//-----------------------------------------------------------------------
+
+//Listen for connections on port 3000
+
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on port 3000...');
 });
