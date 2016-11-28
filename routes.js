c761ef416981a9f1355e93d4faa2a1414ef5c2b1
Steven Low <steven.low@mail.utoronto.ca>
2016-11-28 10:15:57 -0500

    Some reorganization

--- routes.js
+++ routes.js
@@ -4,33 +4,33 @@ var home = require(path.join(__dirname, 'controllers', 'home'));
 
 module.exports = function(app) {
 
-	//Home Page
-    app.get('/', function(req, res) {
-        res.sendFile('views/home/home.html', {root: __dirname});
-    });
+  //Home Page
+  app.get('/', function(req, res) {
+    res.sendFile('views/home/home.html', {root: __dirname});
+  });
 
-    //Chat room page
-	app.get('/chatroom', function(req, res){
-	  res.render('chatRoom/chatRoom.ejs');
-	});
+  //Chat room page
+  app.get('/chatroom', function(req, res){
+    res.render('chatRoom/chatRoom.ejs');
+  });
 
-	//Add in more routes like above...
-	app.post('/register', home.postUser);
-	
-	app.post('/login/signin', home.postSignin);
-	
-	app.get('/login/github', home.getGithub);
-	
-	app.get('/login/google', home.getGoogle);
-	
-	app.get('/callback/github', home.getCallbackGithub);
-	
-	app.get('/callback/google', home.getCallbackGoogle);
-	
-	app.get('/auth/github?', home.getAuthGithub);
-	
-	app.get('/auth/google?', home.getAuthGoogle);
-	
-	app.get('/session', home.getSession);
-	
+  //Add in more routes like above...
+  app.post('/register', home.postUser);
+
+  app.post('/login/signin', home.postSignin);
+
+  app.get('/login/github', home.getGithub);
+
+  app.get('/login/google', home.getGoogle);
+
+  app.get('/callback/github', home.getCallbackGithub);
+
+  app.get('/callback/google', home.getCallbackGoogle);
+
+  app.get('/auth/github?', home.getAuthGithub);
+
+  app.get('/auth/google?', home.getAuthGoogle);
+
+  app.get('/session', home.getSession);
+
 }
